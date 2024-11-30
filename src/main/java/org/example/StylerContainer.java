d92d36b997be159884a176344e8cafb749361ee6
jiangyingying <jiangyingying1031@outlook.com>
2024-11-30 16:51:49 +0800

    sync

--- src/main/java/org/example/StylerContainer.java
+++ src/main/java/org/example/StylerContainer.java
@@ -3,14 +3,10 @@ package org.example;
 import org.example.styler.Styler;
 import org.example.styler.arrangement.ArrangementStyler;
 import org.example.styler.body.braceformat.BraceFormatStyler;
-import org.example.styler.body.layout.BodyLayoutStyler;
-import org.example.styler.body.optionalbrace.OptionalBraceStyler;
 import org.example.styler.format.indention.IndentionStyler;
-import org.example.styler.format.linestmt.LineStmtStyler;
-import org.example.styler.format.linewrapping.LineWrappingStyler;
 import org.example.styler.format.newline.NewlineStyler;
 import org.example.styler.format.space.SpaceStyler;
-import org.example.styler.structure.StructureStyler;
+
 
 import java.util.ArrayList;
 import java.util.List;
