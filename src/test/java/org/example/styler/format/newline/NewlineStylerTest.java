f701c7876a355d7e7d37ed71e8a8c665ffbfa118
jiangyingying <jiangyingying1031@outlook.com>
2025-06-24 18:02:58 +0800

    add test

--- src/test/java/org/example/styler/format/newline/NewlineStylerTest.java
+++ src/test/java/org/example/styler/format/newline/NewlineStylerTest.java
@@ -25,13 +25,13 @@ class NewlineStylerTest extends TestBase {
 		};
 
 		for (int i = 0; i < srcFiles.length; i++) {
-			Path gtPath = Paths.get("src/test/sources/format/newline", String.format("gt%s.java", i + 1));
+			Path gtPath = Paths.get("src/test/sources/format/newline", String.format("gt%s.txt", i + 1));
 			String actual = apply(Paths.get(srcFiles[i]), Paths.get(targetFiles[i]), List.of(NewlineStyler.class, IndentionStyler.class));
-//			try{
-//				Files.writeString(gtPath, actual);
-//			}	catch (Exception e) {
-//				e.printStackTrace();
-//			}
+			try{
+				Files.writeString(gtPath, actual);
+			}	catch (Exception e) {
+				e.printStackTrace();
+			}
 
 			testCodeEqual(actual, gtPath);
 //			break;
