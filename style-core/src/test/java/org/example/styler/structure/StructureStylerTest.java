8386dc480028a919a74ab00aac998add00844d53
jiangyingying <jiangyingying1031@outlook.com>
2025-07-16 18:48:01 +0800

    fix plugin

--- style-core/src/test/java/org/example/styler/structure/StructureStylerTest.java
+++ style-core/src/test/java/org/example/styler/structure/StructureStylerTest.java
@@ -84,11 +84,11 @@ class StructureStylerTest extends TestBase {
 		for (int i = 0; i < srcFiles.length; i++) {
 			Path gtPath = Paths.get(dir, String.format("gt%s.java", i + 1));
 			String actual = apply(Paths.get(dir, srcFiles[i]), Paths.get(dir, targetFiles[i]), List.of(StructureStyler.class));
-			try{
-				Files.writeString(gtPath, actual);
-			}	catch (Exception e) {
-				e.printStackTrace();
-			}
+//			try{
+//				Files.writeString(gtPath, actual);
+//			}	catch (Exception e) {
+//				e.printStackTrace();
+//			}
 
 			testCodeEqual(actual, gtPath);
 		}
