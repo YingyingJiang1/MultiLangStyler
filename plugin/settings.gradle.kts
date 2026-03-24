1699358263b66b6e260345d3692f677d81356ae7
jiangyingying <jiangyingying1031@outlook.com>
2026-03-24 21:48:36 +0800

    remove plugin dir

--- plugin/settings.gradle.kts
+++
@@ -1,5 +0,0 @@
-rootProject.name = "STYLEX Plugin"
-
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
--- plugin/src/main/java/org/example/Utils.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Utils {
-	public static String getMostCommonAuthorInProject() {
-		return "";
-	}
-}
